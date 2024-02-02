bec02059eb66b9c1e0af052a99a6842cdd7f6dd8
Peggy <papax1123@gmail.com>
2024-02-02 10:44:31 +0800

    deploy

---
+++ assets/AboutView-5dUkANj-.js
@@ -0,0 +1,1 @@
+import{_ as e,c as a,o as s,a as n}from"./index-DKr4F7Ed.js";const c={data(){return{data:{}}},mounted(){this.$http.get("import.meta.env.VITE_PATH").then(t=>{console.log(t),console.log("這是部署的環境"),this.data=t.data})}},r={class:"about"},_=n("h1",null,"This is an about page",-1),d=[_];function i(o,t,l,u,p,h){return s(),a("div",r,d)}const f=e(c,[["render",i]]);export{f as default};
